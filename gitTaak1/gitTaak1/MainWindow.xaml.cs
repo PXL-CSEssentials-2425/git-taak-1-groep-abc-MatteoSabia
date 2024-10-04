c434009f07273f1702ab1c5ecfa5294bc2341628
MatteoSabia <12402162@student.pxl.be>
2024-10-04 20:45:11 +0200

    Commit Afbeeldingen toegevoegd en Hover Effect

--- gitTaak1/gitTaak1/MainWindow.xaml.cs
+++ gitTaak1/gitTaak1/MainWindow.xaml.cs
@@ -18,7 +18,35 @@ namespace gitTaak1
     {
         public MainWindow()
         {
+
+       
+
+
             InitializeComponent();
+        }
+
+        private void Rood_MouseEnter(object sender, MouseEventArgs e)
+        {
+            SolidColorBrush red = new SolidColorBrush(Colors.Red);
+            Venster.Background = red;
+        }
+
+        private void Geel_MouseEnter(object sender, MouseEventArgs e)
+        {
+            SolidColorBrush yellow = new SolidColorBrush(Colors.Yellow);
+            Venster.Background = yellow;
+        }
+
+        private void Groen_MouseEnter(object sender, MouseEventArgs e)
+        {
+            SolidColorBrush green = new SolidColorBrush(Colors.Green);
+            Venster.Background = green;
+        }
+
+        private void Blauw_MouseEnter(object sender, MouseEventArgs e)
+        {
+            SolidColorBrush blauw = new SolidColorBrush(Colors.Blue);
+            Venster.Background = blauw;
         }
     }
 }
